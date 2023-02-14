1185d0bc10e7ad01c2494d483a51895eef2ddd12
David <Rigby335@hotmail.com>
2023-02-14 12:51:12 -0800

    WIP using context, not working yet

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,14 +1,34 @@
 import { createContext, useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchPeopleByName, fetchPlanet } from "../fetch-utils";
 
 const SwapiContext = createContext();
 
 export default function DataProvider({ children }) {
   const [planet, setPlanet] = useState([]);
+  const [peopleByName, setPeopleByName] = useState([]);
 
   const value = {
     planet,
     setPlanet,
+    peopleByName,
+    setPeopleByName,
+    getPeopleById,
+    getPlanet,
   };
+
+  const { name } = useParams();
+
+  async function getPeopleById() {
+    const response = await fetchPeopleByName(name);
+    setPeopleByName(response.results[0]);
+  }
+
+  async function getPlanet() {
+    const world = peopleByName.homeworld;
+    const response = await fetchPlanet(world);
+    setPlanet(response);
+  }
 
   return (
     <SwapiContext.Provider value={value}>{children}</SwapiContext.Provider>
--- src/components/PeopleDetail.js
+++ src/components/PeopleDetail.js
@@ -1,39 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { fetchPeopleByName, fetchPlanet } from "../fetch-utils";
+import React, { useEffect } from "react";
+import { useSwapiContext } from "./Context";
 import "../App.css";
 
 export default function PeopleDetail() {
-  const [peopleByName, setPeopleByName] = useState([]);
-  const [planet, setPlanet] = useState([]);
+  const { peopleByName, getPeopleById } = useSwapiContext();
 
   console.log("peopleByName", peopleByName);
-  console.log("planet", planet);
-
-  const { name } = useParams();
-
-  async function getPeopleById() {
-    const response = await fetchPeopleByName(name);
-    // console.log("response", response.results[0]);
-    setPeopleByName(response.results[0]);
-  }
-
-  async function getPlanet() {
-    const world = peopleByName.homeworld;
-    const response = await fetchPlanet(world);
-    setPlanet(response);
-  }
 
   useEffect(() => {
-    getPeopleById();
-    getPlanet();
+    async function test() {
+      const res = await getPeopleById();
+      console.log("res", res);
+    }
+    test();
   }, []);
 
   return (
     <div className="detail">
       <div className="detail-card">
         <h2>{peopleByName.name}</h2>
-        <p>Homeworld: {planet.name}</p>
+        <p>Homeworld: </p>
       </div>
     </div>
   );
